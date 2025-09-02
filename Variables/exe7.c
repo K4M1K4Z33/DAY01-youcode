da965f1f5ce942c5977374bc7946a37ee93f1574
Kamikaze <ouyachouo94@gmail.com>
2025-09-02 17:26:03 +0100

    done

---
+++ Variables/exe7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c, moyenne;
+    
+    printf("1er Nombre: ");
+    scanf("%f", &a);
+    printf("2éme Nombre: ");
+    scanf("%f", &b);
+    printf("3éme Nombre: ");
+    scanf("%f", &c);
+    
+    moyenne = (a*2 + b*3 + c*5) / 10;
+    printf("Moyenne: %.2f", moyenne);
+    
+    return 0;
+}
